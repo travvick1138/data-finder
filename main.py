0354af654428c90993c0c1fe17a386693eee971a
Travis Vick <Travis Vick>
2016-05-23 16:12:33 -0700

    travis52316

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ import json
 
 with open ('data.json', 'r') as f:
     contents = f.read()
-    
+    data = json.loads(contents)
+
+def main():
+
+    for category in main.keys():
+        print(main[category])
+    user_input = input('What category would you like to explore')
