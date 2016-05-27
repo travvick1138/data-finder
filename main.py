9718dd579d5492c3a459b16dceba485c587a2145
Travis Vick <Travis Vick>
2016-05-26 18:55:36 -0700

    Travis

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ with open('data.json', 'r') as f:
     contents = f.read()
     data = json.loads(contents)
 
+def search_list_item_and_return_parent_key_and_child_key(category, list_item):
+    """find a ingredient and return parent and child keys
+
+    """
+    if list_item in data[category]["data"]:
+        print(list_item)
+
 
 def build_a_string(category):
     """Build strings for first_prompt and pulling in any data as needed
@@ -17,13 +24,13 @@ def build_a_string(category):
     >>>build_a_string("Food")
     'Enter the name of a cuisine ('Thai', 'Mexican', 'Italian', 'Indian') or an ingredient: '
     """
-
     value_list = list(data[category]["data"].keys())
     value_list.sort()
 
     return data[category]["strings"]['first_prompt'].format(
         parent_keys=', '.join(value_list)
-     )
+    )
+
 # def build_a_string(**kwargs):
 #     if kwargs in data.keys():
 #         category = kwargs
@@ -56,9 +63,9 @@ def build_a_string(category):
 #         print("We do not have the information you are looking for, please try again.")
 #         main()
 
-
 def first_prompt(key):
     user_input = input(build_a_string(key))
+
 
     second_prompt(key, user_input)
 
@@ -71,12 +78,18 @@ def build_another_string(category, parent_key):
     >>>build_another_string("Food", "Indian")
     'Enter the name of a dish ('Palak Paneer'): '
     """
-    value_list = list(data[category]["data"][parent_key].keys())
-    value_list.sort()
 
-    return data[category]["strings"]['second_prompt'].format(
-        child_keys=', '.join(value_list)
-    )
+    if parent_key in data[category].keys():
+        value_list = list(data[category]["data"][parent_key].keys())
+        value_list.sort()
+
+        return data[category]["strings"]['second_prompt'].format(
+            child_keys=', '.join(value_list)
+        )
+    elif parent_key not in data[category].keys():
+        search_list_item_and_return_parent_key_and_child_key(category, parent_key)
+
+
 
 
 def second_prompt(category, parent_key):
@@ -104,41 +117,35 @@ def output(category, parent_key, child_key):
     final_prompt()
 
 def final_prompt():
-    """for users to continue search"""
-    y = True
-    # n = True
-
-    user_input = input("Would you like another search? (y or n)")
-
-    if user_input == y:
-                main()
-    # elif user_input == n:
-    #             break
-
-
-
+    # y = True
+    while True:
+        user_input = input("Would you like another search? (y or n)")
+        if user_input == "y":
+            main()
+        else:
+            break
 
 
 def main():
     """ main gets the initial category imported from data:
 
     """
-    for category in data.keys():
-        keys = list(data.keys())
-        keys.sort()
+    keys = list(data.keys())
+    keys.sort()
+    import sys
+    #while True:
+    user_input = input('What category would you like to explore? ({data}): '.format(
 
-        user_input = input('What category would you like to explore? ({data}): '.format(
+        data=', '.join(keys)
+    ))
+    if user_input in keys:
+        first_prompt(user_input)
+    elif user_input not in keys:
+        print("We do not have the information you are looking for, please try again.")
+        main()
+        # #else:
+        #     None
 
-            data=', '.join(keys)
-        ))
-        if user_input in keys:
-            first_prompt(user_input)
-        elif user_input not in keys:
-            print("We do not have the information you are looking for, please try again.")
-            main()
-        else:
-            print("Goodbye")
-            return None
 
 main()
 # if __name__ == '__main__':
